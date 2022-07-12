966f54730fa2ac8af00399fcd208c1745e89255b
suzzette211 <susanndambuki211@gmail.com>
2022-07-11 20:26:38 -0700

    my function to update the reference of a pointer

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,19 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
-* reset_to_98 -> a function to update the reference of a pointer
-* @n: pointer to n to be updated
-*/
-void reset_to_98(int *n)
-{
-README.md *n = 98
-}
+#include <stdio.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif /* MAIN_H */
