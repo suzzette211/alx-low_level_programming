800a48896e2302dc1d3eadc71659fb2173967a13
suzzette211 <susanndambuki211@gmail.com>
2022-07-14 06:47:59 -0700

    my function that prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,12 +13,12 @@ int a = 0, b = 0, c = 0, d = 0;
 
 while (x < 1440)
 {
-_putchar(a + 0);
-_putchar(b + 0);
-_putchar(:);
-_putchar(c + 0);
-_putchar(d + 0);
-_putchar(n);
+_putchar(a + '0');
+_putchar(b + '0');
+_putchar(':');
+_putchar(c + '0');
+_putchar(d + '0');
+_putchar('\n');
 
 d++;
 if (d > 9)
